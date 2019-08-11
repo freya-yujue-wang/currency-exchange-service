1cf12e3767229f6c1acae9d48d21878f26d06b0b
wyjsusan <yujuewa@gmail.com>
2019-08-10 22:02:46 -0700

    Reformat and clean up.

--- src/main/java/com/amituofo/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/amituofo/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,5 @@
 package com.amituofo.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
